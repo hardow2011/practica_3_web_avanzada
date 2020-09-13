68b1472dcf70624db8cd5566cbfcbcc21f143041
hardow2011 <hardow2011@outlook.com>
2020-09-12 22:44:07 -0400

    Proyecto creado, entidad de estudiante creada y servicio de estudiante en proceso.

---
+++ src/main/java/com/example/practica_1/entidades/Estudiante.java
@@ -0,0 +1,57 @@
+package com.example.practica_1.entidades;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Estudiante implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String matricula;
+    private String nombre;
+    private String telefono;
+    
+    public Estudiante(String matricula, String nombre, String telefono) {
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+}
---
+++ src/main/java/com/example/practica_1/servicios/EstudianteServices.java
@@ -0,0 +1,8 @@
+package com.example.practica_1.servicios;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EstudianteServices {
+    
+}
